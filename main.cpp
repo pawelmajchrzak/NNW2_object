2ce31dec7b7152f3a7bb082d2520a644fbeb46b4
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2022-10-04 23:16:15 +0200

    Create class Liczby with constructor

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class Liczby
+{
+    private:
+    vector <int> liczby;
+
+    public:
+    Liczby (int iloscLiczb)
+    {
+        liczby.push_back(iloscLiczb);
+        for (int i=0; i<iloscLiczb; i++)
+        {
+            cin >> liczby[i];
+        }
+    }
+    void wyznaczNNW()
+    {
+        cout <<liczby[0] << endl;
+        cout <<liczby[1] << endl;
+        cout <<liczby[2] << endl;
+    }
+};
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int iloscTestow;
+    cin>>iloscTestow;
+
+    for (int i=1 ; i <= iloscTestow ; i++)
+        {
+            int iloscLiczb;
+            cin >> iloscLiczb;
+            Liczby zestaw(iloscLiczb);
+            zestaw.wyznaczNNW();
+        }
     return 0;
 }
